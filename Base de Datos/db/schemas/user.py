c38e19cd5cca6b31f8a3a9e8fd088e6a549faa8a
ferdher <43883334+ferdher@users.noreply.github.com>
2023-05-06 08:48:13 -0600

    Add files via upload

---
+++ Base de Datos/db/schemas/user.py
@@ -0,0 +1,6 @@
+def user_schema(user) -> dict:
+
+    return {"id": str (user["_id"]),            
+            "username": user["username"], 
+            "full_name":user["full_name"],
+            "email": user["email"]}
